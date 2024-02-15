d88bf428d6cc1b1d1e45aae0dc00d9667b117c11
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-02-15 15:38:16 +0530

    added signup page

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-                           
+const loginBt = document.getElementById("loginbt");
+const signupBt = document.getElementById("signupbt");
+const loginFunc = document.getElementById("logindiv");
+const signupFunc = document.getElementById("signupdiv");
+
+loginBt.addEventListener("click", function () {
+  loginFunc.style.display = "block";
+  signupFunc.style.display = "none";
+});
+signupBt.addEventListener("click", () => {
+  loginFunc.style.display = "none";
+  signupFunc.style.display = "block";
+});
+
+// photo preview code
+function change() {
+  const fileInput = document.getElementById("file");
+  const previewImgs = document.querySelectorAll(".profile-img");
+
+  const selectedFile = fileInput.files[0];
+
+  if (selectedFile && selectedFile.type.startsWith("image/")) {
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      previewImgs.forEach((element) => {
+        element.src = event.target.result;
+      });
+    };
+    reader.readAsDataURL(selectedFile);
+    previewImgs.forEach((element) => {
+      element.style.display = "block";
+    });
+  } else {
+    previewImgs.forEach((element) => {
+      element.style.display = "none";
+    });
+  }
+}
