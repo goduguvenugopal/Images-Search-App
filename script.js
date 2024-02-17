614ce27a6fa5a27535a7ea281377509177bcf750
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-02-17 12:58:16 +0530

    add

--- script.js
+++ script.js
@@ -63,9 +63,9 @@ formFunc.addEventListener("submit", function (event) {
     "userData",
     JSON.stringify({ name: storeName, email: storeEmail, password: storePass })
   );
-
+  const userData1 = JSON.parse(localStorage.getItem("userData"));
   outName.forEach((element) => {
-    element.textContent = `Hi, ${storeName}`;
+    element.textContent = `Hi, ${userData1.storeName}`;
   });
 
   alert("Your Account Has Been Created , Now Login The Website ");
@@ -134,77 +134,7 @@ function openMain() {
   mainPage.style.display = "block";
   logoutDiv.style.display = "block";
 }
-
-// signup form code
-
-// const formFunc = document.getElementById("signupform");
-
-// const userName = document.getElementById("username");
-// const outName = document.querySelectorAll(".navbar-name");
-
-// formFunc.addEventListener("submit", function (x) {
-//   x.preventDefault();
-
-//   const storeName = userName.value;
-//   outName.forEach((x) => {
-//     x.textContent = `Hi,${storeName}`;
-//   });
-
-//   alert("Your Account Has Been Created , Now Login The Website ");
-// });
-
-// // login code
-
-// const emailText = document.getElementById("email");
-// const passWord = document.getElementById("signup-pass");
-// const logPage = document.getElementById("log-in-page");
-// const formLog = document.getElementById("loginform");
-// const logEmail = document.getElementById("login-email");
-// const logPass = document.getElementById("login-pass");
-// const mainPage = document.getElementById("mainpage");
-// const logoutDiv = document.getElementById("log-pro-cont");
-
-// formLog.addEventListener("submit", function (l) {
-//   l.preventDefault();
-
-//   const storelog = logEmail.value;
-//   const getEmail = emailText.value;
-//   const storePass = logPass.value;
-//   const getPass = passWord.value;
-
-//   if (storelog === getEmail && storePass === getPass) {
-//     setTimeout(openMain, 1000);
-//   } else {
-//     alert(
-//       "Your Email and Password is wrong , Please SignUp If You Don't Have An Account"
-//     );
-//   }
-// });
-
-// function openMain() {
-//   logPage.style.display = "none";
-//   mainPage.style.display = "block";
-//   logoutDiv.style.display = "block";
-// }
-
-// // logout function
-
-// const logoutButton = document.querySelectorAll(".log-out-bt");
-// Array.from(logoutFunc).forEach((x) => {
-//   x.addEventListener("click", () => {
-//     localStorage.removeItem("isLoggedIn");
-//     logPage.style.display = "block"; // Show login page again
-//     mainPage.style.display = "none"; // Hide main page
-//     logoutDiv.style.display = "none"; // Hide logout div
-//   });
-// });
-
-// function openlog() {
-//   logPage.style.display = "block";
-//   mainPage.style.display = "none";
-//   logoutDiv.style.display = "none";
-// }
-
+ 
 // images API code
 
 const accessKey = "k49MRcGfcPvJ3zb1pznNYw97rO_hlCGcn0GPHZ-hUkM";
